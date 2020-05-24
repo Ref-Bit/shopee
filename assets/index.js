141d787a4fae7af3ca8542950543fe47a95c2bee
Ref-Bit <rsv.gho@gmail.com>
2020-05-24 17:56:41 +0300

    Banner Carousel is added

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#banner .owl-carousel").owlCarousel({
+    dots: true,
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayHoverPause: 3000,
+  })
+});
