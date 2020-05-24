ae61b404fef5e490309ca93973a45c4184abdf03
Ref-Bit <rsv.gho@gmail.com>
2020-05-24 18:47:35 +0300

    Top Sale section is added

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  /* Banner Carousel */
   $("#banner .owl-carousel").owlCarousel({
     dots: true,
     items: 1,
@@ -6,5 +7,27 @@ $(document).ready(function() {
     autoplay: true,
     autoplayHoverPause: true,
     autoplayHoverPause: 3000,
+  })
+
+  /* Top Sale Carousel */
+  $("#top-sale .owl-carousel").owlCarousel({
+    nav: true,
+    dots: false,
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayHoverPause: 3000,
+    responsive:{
+      0:{
+        items: 1
+      },
+      600:{
+        items: 3
+      },
+      1000:{
+        items: 5
+      }
+    }
   })
 });
